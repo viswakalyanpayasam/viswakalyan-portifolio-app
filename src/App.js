76b78112a2878d859adaae23c0bdee0e03b088a9
viswakalyanpayasam <viswakalyan323@gmail.com>
2023-05-06 17:27:02 +0530

    portifolio updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Experience from "./Components/Experience";
+import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div>
      <NavBar/>
+     <Home/>
+     <About/>
+     <Experience/>
+     <Contact/>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,37 +8,45 @@ const NavBar = () => {
     const link = [
         {
             id: 1,
-            link: 'Home'
+            link: 'home'
         },
         {
             id: 2,
             link: 'about'
         },
         {
-            id: 1,
-            link: 'portifolio'
-        },
-        {
-            id: 1,
+            id: 3,
             link: 'experience'
         },
         {
-            id: 1,
+            id: 4,
             link: 'contact'
         }
     ]
     return (
         <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
             <div>
-                <h1 className='text-5xl font-signature ml-2'>Viswa-Kalyan</h1>
+                <Link
+
+                    to='home'
+                    smooth
+                    duration={500}
+                >
+                    <h1 className='text-5xl font-signature cursor-pointer ml-2'>Viswa Kalyan</h1>
+                </Link>
             </div>
-            <ul className='hidden md:flex'>
-                {link.map(({ id, link }) =>
-                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link} </li>
-                )}
-
+            <ul className="hidden md:flex">
+                {link.map(({ id, link }) => (
+                    <li
+                        key={id}
+                        className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+                    >
+                        <Link to={link} smooth duration={500}>
+                            {link}
+                        </Link>
+                    </li>
+                ))}
             </ul>
-
             <div
                 onClick={() => setNav(!nav)}
                 className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
